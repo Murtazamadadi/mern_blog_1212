8414cf06a525be6f8c552374784bf6f6224eb902
MutazaMadadi <murtazamadadi12@gmail.com>
2024-05-11 09:46:47 +0330

    create pagas and routes for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,28 @@
 
 import './App.css'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+
+
+import Home from "./pages/Home"
+import Aboute from './pages/Aboute'
+import Dashboard from "./pages/Dashboard"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Projects from "./pages/Projects"
+
 
 function App() {
- 
-
   return (
-   <div className=' text-red-500 font-bold'>hello</div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<Aboute/>}/>
+    <Route path='/dashboard' element={<Dashboard/>}/>
+    <Route path='/sign-in' element={<SignIn/>}/>
+    <Route path='/sign-up' element={<SignUp/>}/>
+    <Route path='/projects' element={<Projects/>}/>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
